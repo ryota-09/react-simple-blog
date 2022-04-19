92809ab98eef776d992da4984f775dd323362980
ryota-09 <n.ryo950911@gmail.com>
2022-04-19 16:02:42 +0900

    ariticleクラスの作成

---
+++ src/hooks/useAllArticles.ts
@@ -0,0 +1,6 @@
+import { useState } from "react";
+
+export const useAllArticleList = () => {
+  const [ articleList, setArticleList ] = useState([]);
+  return {}
+}
---
+++ src/types/article.ts
@@ -0,0 +1,40 @@
+export class Article {
+  constructor(
+    private _id: string,
+    private _title: string,
+    private _body: string,
+    private _date: string
+  ) {}
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set id(id: string) {
+    this._id = id;
+  }
+
+  public get title(): string {
+    return this._title;
+  }
+
+  public set title(title: string) {
+    this._title = title;
+  }
+
+  public get body(): string {
+    return this._body;
+  }
+
+  public set body(body: string) {
+    this._body = body;
+  }
+
+  public get date(): string {
+    return this._date;
+  }
+
+  public set date(date: string) {
+    this._date = date;
+  }
+}
